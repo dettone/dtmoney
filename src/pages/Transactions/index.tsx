8ef74fd1aaaabe7d683a96e64415233e5fc08cc3
dettone <dettonex25@gmail.com>
2023-02-09 23:20:30 -0300

    List and Search HTML and Styles

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../../components/Header";
+import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions(){
@@ -7,6 +9,38 @@ export function Transactions(){
         <div>
            <Header /> 
            <Summary />
+        <TransactionContainer>
+            <SearchForm />
+           <TransactionsTable>
+            <tbody>
+                <tr>
+                    <td width="50%"> Desenvolvimento de site </td>
+                    <td><PriceHighLight variant = 'income'> R$ 20.200,00 </PriceHighLight></td>
+                    <td> Venda </td>
+                    <td> 13/04/2022</td>
+                </tr>
+                <tr>
+                    <td width="50%"> Hambúrguer </td>
+                    <td> <PriceHighLight variant = 'outcome'>- R$ 2.200,00 </PriceHighLight></td>
+                    <td> Alimentação </td>
+                    <td> 13/04/2022</td>
+                </tr>
+                <tr>
+                    <td width="50%"> Desenvolvimento de site </td>
+                    <td> <PriceHighLight variant = 'income'> R$ 20.200,00 </PriceHighLight> </td>
+                    <td> Venda </td>
+                    <td> 13/04/2022</td>
+                </tr>
+                <tr>
+                    <td width="50%"> Desenvolvimento de site </td>
+                    <td> <PriceHighLight variant = 'income'> R$ 20.200,00 </PriceHighLight> </td>
+                    <td> Venda </td>
+                    <td> 13/04/2022</td>
+                </tr>
+            </tbody>
+           </TransactionsTable>
+                       
+        </TransactionContainer>
         </div>
     )
 }
